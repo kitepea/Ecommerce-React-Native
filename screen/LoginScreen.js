846a3313e336cf2e79bc079d342185144f5d0ffe
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-06-12 00:02:32 +0700

    Add: Login Screen UI

--- screen/LoginScreen.js
+++ screen/LoginScreen.js
@@ -1,14 +1,99 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  KeyboardAvoidingView,
+  TextInput,
+} from "react-native";
+import React from "react";
+import SafeViewAndroid from "../components/SafeViewAndroid";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const LoginScreen = () => {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "white",
+      alignItems: "center",
+      marginTop: 50,
+    },
+  });
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={[styles.container, SafeViewAndroid.AndroidSafeArea]}>
+      <Image
+        style={{ width: 150, height: 150 }}
+        source={require("../assets/w.png")}
+      />
 
-export default LoginScreen
+      <KeyboardAvoidingView>
+        <View style={{ alignItems: "center" }}>
+          <Text
+            style={{
+              fontSize: 17,
+              fontWeight: "bold",
+              marginTop: 12,
+              color: "#041E42",
+            }}
+          >
+            Login to your account
+          </Text>
 
-const styles = StyleSheet.create({})
+          <View style={{ marginTop: 70 }}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 5,
+                backgroundColor: "#D0D0D0",
+                paddingVertical: 5,
+                borderRadius: 5,
+                marginTop: 30,
+              }}
+            >
+              <MaterialIcons
+                style={{ marginHorizontal: 8 }}
+                name="email"
+                size={24}
+                color="#808080"
+              />
+              <TextInput
+                style={{ width: 300, color: "#808080", marginVertical: 10 }}
+                placeholder="Enter your email"
+              ></TextInput>
+            </View>
+          </View>
+
+          <View style={{ marginTop: 30 }}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 5,
+                backgroundColor: "#D0D0D0",
+                paddingVertical: 5,
+                borderRadius: 5,
+              }}
+            >
+              <MaterialIcons
+                style={{ marginHorizontal: 8 }}
+                name="password"
+                size={24}
+                color="#808080"
+              />
+              <TextInput
+                style={{ width: 300, color: "#808080", marginVertical: 10 }}
+                placeholder="Enter your password"
+              ></TextInput>
+            </View>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({});
