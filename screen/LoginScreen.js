5401c223e0935e8b433f9882efa6b397ff7a0118
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-06-12 15:54:46 +0700

    Done: Login screen

--- screen/LoginScreen.js
+++ screen/LoginScreen.js
@@ -6,12 +6,15 @@ import {
   Image,
   KeyboardAvoidingView,
   TextInput,
+  Pressable,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import SafeViewAndroid from "../components/SafeViewAndroid";
 import { MaterialIcons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 const LoginScreen = () => {
+  const navigation = useNavigation();
   const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -20,6 +23,10 @@ const LoginScreen = () => {
       marginTop: 50,
     },
   });
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <SafeAreaView style={[styles.container, SafeViewAndroid.AndroidSafeArea]}>
       <Image
@@ -31,7 +38,7 @@ const LoginScreen = () => {
         <View style={{ alignItems: "center" }}>
           <Text
             style={{
-              fontSize: 17,
+              fontSize: 23,
               fontWeight: "bold",
               marginTop: 12,
               color: "#041E42",
@@ -40,7 +47,7 @@ const LoginScreen = () => {
             Login to your account
           </Text>
 
-          <View style={{ marginTop: 70 }}>
+          <View style={{ marginTop: 30 }}>
             <View
               style={{
                 flexDirection: "row",
@@ -59,7 +66,14 @@ const LoginScreen = () => {
                 color="#808080"
               />
               <TextInput
-                style={{ width: 300, color: "#808080", marginVertical: 10 }}
+                value={email}
+                onChangeText={(text) => setEmail(text)}
+                style={{
+                  width: 300,
+                  color: "#808080",
+                  marginVertical: 10,
+                  fontSize: email ? 16 : 16,
+                }}
                 placeholder="Enter your email"
               ></TextInput>
             </View>
@@ -83,12 +97,66 @@ const LoginScreen = () => {
                 color="#808080"
               />
               <TextInput
-                style={{ width: 300, color: "#808080", marginVertical: 10 }}
+                value={password}
+                onChangeText={(text) => setPassword(text)}
+                secureTextEntry={true}
+                style={{
+                  width: 300,
+                  color: "#808080",
+                  marginVertical: 10,
+                  fontSize: password ? 16 : 16,
+                }}
                 placeholder="Enter your password"
               ></TextInput>
             </View>
           </View>
         </View>
+
+        <View
+          style={{
+            marginTop: 12,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text>Keep logged in</Text>
+          <Text style={{ color: "#007FFF", fontWeight: 500 }}>
+            Forgot password
+          </Text>
+        </View>
+
+        <View style={{ marginTop: 50 }} />
+
+        <Pressable
+          style={{
+            width: 180,
+            backgroundColor: "#5B92E5",
+            borderRadius: 6,
+            marginHorizontal: "auto",
+            padding: 13,
+          }}
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              fontSize: 19,
+              fontWeight: "bold",
+              color: "white",
+            }}
+          >
+            Log In
+          </Text>
+        </Pressable>
+
+        <Pressable
+          onPress={() => navigation.navigate("Register")}
+          style={{ marginTop: 10 }}
+        >
+          <Text style={{ textAlign: "center", color: "gray", fontSize: 16 }}>
+            Don't have account yet? Sign up
+          </Text>
+        </Pressable>
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
