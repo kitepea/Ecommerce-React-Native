a3a4c539708529076221e9fd20c40aad9e155622
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-06-14 09:12:56 +0700

    Fix: KeyboardAvoidingView

--- screen/RegisterScreen.js
+++ screen/RegisterScreen.js
@@ -8,6 +8,7 @@ import {
   TextInput,
   Pressable,
   Alert,
+  ScrollView,
 } from "react-native";
 import React, { useState } from "react";
 import SafeViewAndroid from "../components/SafeViewAndroid";
@@ -61,155 +62,159 @@ const RegisterScreen = () => {
       });
   };
   return (
-    <SafeAreaView style={[styles.container, SafeViewAndroid.AndroidSafeArea]}>
-      <Image
-        style={{ width: 150, height: 150 }}
-        source={require("../assets/w.png")}
-      />
+    <KeyboardAvoidingView>
+      <ScrollView>
+        <SafeAreaView
+          style={[styles.container, SafeViewAndroid.AndroidSafeArea]}
+        >
+          <Image
+            style={{ width: 150, height: 150 }}
+            source={require("../assets/w.png")}
+          />
 
-      <KeyboardAvoidingView>
-        <View style={{ alignItems: "center" }}>
-          <Text
+          <View style={{ alignItems: "center" }}>
+            <Text
+              style={{
+                fontSize: 23,
+                fontWeight: "bold",
+                marginTop: 12,
+                color: "#041E42",
+              }}
+            >
+              Register to your account
+            </Text>
+
+            <View style={{ marginTop: 30 }}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  gap: 5,
+                  backgroundColor: "#D0D0D0",
+                  paddingVertical: 5,
+                  borderRadius: 5,
+                  marginTop: 30,
+                }}
+              >
+                <MaterialIcons
+                  style={{ marginHorizontal: 8 }}
+                  name="person"
+                  size={24}
+                  color="#808080"
+                />
+                <TextInput
+                  value={name}
+                  onChangeText={(text) => setName(text)}
+                  style={{
+                    width: 300,
+                    color: "#808080",
+                    marginVertical: 10,
+                    fontSize: name ? 16 : 16,
+                  }}
+                  placeholder="Enter your name"
+                ></TextInput>
+              </View>
+            </View>
+
+            <View style={{ marginTop: 30 }}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  gap: 5,
+                  backgroundColor: "#D0D0D0",
+                  paddingVertical: 5,
+                  borderRadius: 5,
+                }}
+              >
+                <MaterialIcons
+                  style={{ marginHorizontal: 8 }}
+                  name="email"
+                  size={24}
+                  color="#808080"
+                />
+                <TextInput
+                  value={email}
+                  onChangeText={(text) => setEmail(text)}
+                  style={{
+                    width: 300,
+                    color: "#808080",
+                    marginVertical: 10,
+                    fontSize: email ? 16 : 16,
+                  }}
+                  placeholder="Enter your email"
+                ></TextInput>
+              </View>
+            </View>
+
+            <View style={{ marginTop: 30 }}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  gap: 5,
+                  backgroundColor: "#D0D0D0",
+                  paddingVertical: 5,
+                  borderRadius: 5,
+                }}
+              >
+                <MaterialIcons
+                  style={{ marginHorizontal: 8 }}
+                  name="password"
+                  size={24}
+                  color="#808080"
+                />
+                <TextInput
+                  value={password}
+                  onChangeText={(text) => setPassword(text)}
+                  secureTextEntry={true}
+                  style={{
+                    width: 300,
+                    color: "#808080",
+                    marginVertical: 10,
+                    fontSize: password ? 16 : 16,
+                  }}
+                  placeholder="Enter your password"
+                ></TextInput>
+              </View>
+            </View>
+          </View>
+
+          <View style={{ marginTop: 50 }} />
+
+          <Pressable
+            onPress={handleRegister}
             style={{
-              fontSize: 23,
-              fontWeight: "bold",
-              marginTop: 12,
-              color: "#041E42",
+              width: 180,
+              backgroundColor: "#5B92E5",
+              borderRadius: 6,
+              marginHorizontal: "auto",
+              padding: 13,
             }}
           >
-            Register to your account
-          </Text>
-
-          <View style={{ marginTop: 30 }}>
-            <View
+            <Text
               style={{
-                flexDirection: "row",
-                alignItems: "center",
-                gap: 5,
-                backgroundColor: "#D0D0D0",
-                paddingVertical: 5,
-                borderRadius: 5,
-                marginTop: 30,
+                textAlign: "center",
+                fontSize: 19,
+                fontWeight: "bold",
+                color: "white",
               }}
             >
-              <MaterialIcons
-                style={{ marginHorizontal: 8 }}
-                name="person"
-                size={24}
-                color="#808080"
-              />
-              <TextInput
-                value={name}
-                onChangeText={(text) => setName(text)}
-                style={{
-                  width: 300,
-                  color: "#808080",
-                  marginVertical: 10,
-                  fontSize: name ? 16 : 16,
-                }}
-                placeholder="Enter your name"
-              ></TextInput>
-            </View>
-          </View>
+              Register
+            </Text>
+          </Pressable>
 
-          <View style={{ marginTop: 30 }}>
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                gap: 5,
-                backgroundColor: "#D0D0D0",
-                paddingVertical: 5,
-                borderRadius: 5,
-              }}
-            >
-              <MaterialIcons
-                style={{ marginHorizontal: 8 }}
-                name="email"
-                size={24}
-                color="#808080"
-              />
-              <TextInput
-                value={email}
-                onChangeText={(text) => setEmail(text)}
-                style={{
-                  width: 300,
-                  color: "#808080",
-                  marginVertical: 10,
-                  fontSize: email ? 16 : 16,
-                }}
-                placeholder="Enter your email"
-              ></TextInput>
-            </View>
-          </View>
-
-          <View style={{ marginTop: 30 }}>
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                gap: 5,
-                backgroundColor: "#D0D0D0",
-                paddingVertical: 5,
-                borderRadius: 5,
-              }}
-            >
-              <MaterialIcons
-                style={{ marginHorizontal: 8 }}
-                name="password"
-                size={24}
-                color="#808080"
-              />
-              <TextInput
-                value={password}
-                onChangeText={(text) => setPassword(text)}
-                secureTextEntry={true}
-                style={{
-                  width: 300,
-                  color: "#808080",
-                  marginVertical: 10,
-                  fontSize: password ? 16 : 16,
-                }}
-                placeholder="Enter your password"
-              ></TextInput>
-            </View>
-          </View>
-        </View>
-
-        <View style={{ marginTop: 50 }} />
-
-        <Pressable
-          onPress={handleRegister}
-          style={{
-            width: 180,
-            backgroundColor: "#5B92E5",
-            borderRadius: 6,
-            marginHorizontal: "auto",
-            padding: 13,
-          }}
-        >
-          <Text
-            style={{
-              textAlign: "center",
-              fontSize: 19,
-              fontWeight: "bold",
-              color: "white",
-            }}
+          <Pressable
+            onPress={() => navigation.navigate("Login")}
+            style={{ marginTop: 10 }}
           >
-            Register
-          </Text>
-        </Pressable>
-
-        <Pressable
-          onPress={() => navigation.navigate("Login")}
-          style={{ marginTop: 10 }}
-        >
-          <Text style={{ textAlign: "center", color: "gray", fontSize: 16 }}>
-            Already have an account? Sign In
-          </Text>
-        </Pressable>
-      </KeyboardAvoidingView>
-    </SafeAreaView>
+            <Text style={{ textAlign: "center", color: "gray", fontSize: 16 }}>
+              Already have an account? Sign In
+            </Text>
+          </Pressable>
+        </SafeAreaView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
